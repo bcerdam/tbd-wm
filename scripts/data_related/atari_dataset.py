ac6a0b91c7a4244b8649249eec698d5adc815384
bcerdam <bcmardini@gmail.com>
2026-03-04 15:19:10 -0300

    autoencoder debug

--- scripts/data_related/atari_dataset.py
+++ scripts/data_related/atari_dataset.py
@@ -9,7 +9,6 @@ class AtariDataset(Dataset):
     def __init__(self, sequence_length:int) -> None:
 
         self.sequence_length = sequence_length
-        self.valid_indices = np.array([], dtype=np.int64)
 
         self.observations = None
         self.actions = None
@@ -37,28 +36,14 @@ class AtariDataset(Dataset):
             self.terminations = np.concatenate([self.terminations, terminations], axis=0)
             self.episode_starts = np.concatenate([self.episode_starts, episode_starts], axis=0)
 
-        total_samples = self.observations.shape[0]
-        invalid_mask = np.zeros(total_samples, dtype=bool)
-        start_indices = np.where(self.episode_starts)[0]
-
-        for t in start_indices:
-            start_range = max(0, t - self.sequence_length + 1)
-            end_range = t
-            invalid_mask[start_range : end_range] = True
-
-        invalid_mask[-self.sequence_length + 1:] = True
-        all_indices = np.arange(total_samples)
-        self.valid_indices = all_indices[~invalid_mask]
-
 
     def __len__(self):
-        return len(self.valid_indices)
+        print(self.observations.shape, len(self.observations))
+        return len(self.observations)-self.sequence_length
     
 
     def __getitem__(self, index:int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-        real_idx = self.valid_indices[index]
-        end_index = real_idx+self.sequence_length
-        return (self.observations[real_idx:end_index], 
-                self.actions[real_idx:end_index], 
-                self.rewards[real_idx:end_index], 
-                self.terminations[real_idx:end_index])
+        return (self.observations[index:self.sequence_length], 
+                self.actions[index:self.sequence_length], 
+                self.rewards[index:self.sequence_length], 
+                self.terminations[index:self.sequence_length])
