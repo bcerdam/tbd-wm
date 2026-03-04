d8d0e80c8641c162c38b356012eab9daf6764711
bcerdam <bcmardini@gmail.com>
2026-03-04 17:20:17 -0300

    autoencoder debugging

--- scripts/models/categorical_vae/categorical_autoencoder_step.py
+++ scripts/models/categorical_vae/categorical_autoencoder_step.py
@@ -5,6 +5,17 @@ from .encoder import CategoricalEncoder
 from .decoder import CategoricalDecoder
 from .sampler import sample
 
+import torch.nn as nn
+from einops import rearrange, repeat, reduce
+
+class MSELoss(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, obs_hat, obs):
+        loss = (obs_hat - obs)**2
+        loss = reduce(loss, "B L C H W -> B L", "sum")
+        return loss.mean()
 
 def autoencoder_fwd_step(categorical_encoder:CategoricalEncoder, 
                          categorical_decoder:CategoricalDecoder, 
@@ -17,6 +28,7 @@ def autoencoder_fwd_step(categorical_encoder:CategoricalEncoder,
     
     categorical_encoder.train()
     categorical_decoder.train()
+    mse_loss = MSELoss()
 
     with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
         latents_batch = categorical_encoder.forward(observations_batch=observations_batch, 
@@ -38,6 +50,7 @@ def autoencoder_fwd_step(categorical_encoder:CategoricalEncoder,
         
         # reconstruction_loss = F.mse_loss(reconstructed_observations_batch, wm_observations_batch)
         reconstruction_loss = ((reconstructed_observations_batch - observations_batch) ** 2).sum(dim=(-3, -2, -1)).mean()
+        reconstruction_loss = mse_loss.forward(obs_hat=reconstructed_observations_batch, obs=observations_batch)
         # perceptual_loss = lpips_loss_fn(wm_observations_batch.view(-1, 3, 64, 64), reconstructed_observations_batch.view(-1, 3, 64, 64)).mean()
         # reconstruction_loss = reconstruction_loss + 0.2 * perceptual_loss
     
